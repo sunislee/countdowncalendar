737fef1688c2107309b5b407e5893c6984234a61
Sunis <iamlishang@126.com>
2016-03-28 23:33:13 +0800

    commit files

---
+++ app/src/main/java/sunis/com/countdowncalendar/application/MyApplication.java
@@ -0,0 +1,9 @@
+package sunis.com.countdowncalendar.application;
+
+import android.app.Application;
+
+/**
+ * Created by Sunis on 2016/3/28.
+ */
+public class MyApplication extends Application{
+}
